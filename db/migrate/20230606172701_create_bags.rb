35413d5ef68f8103e18379482c116f84f5e3655e
michael-mbugua <kamirimichael369@gmail.com>
2023-06-07 12:11:16 +0300

    create bags table

--- db/migrate/20230606172701_create_bags.rb
+++ db/migrate/20230606172701_create_bags.rb
@@ -1,8 +1,13 @@
 class CreateBags < ActiveRecord::Migration[7.0]
   def change
     create_table :bags do |t|
+      t.string :type
       t.string :name
+      t.string :image
       t.integer :price
+      t.boolean :available
+      t.text :description
+
 
       t.timestamps
     end
