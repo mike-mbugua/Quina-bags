7db80fcfec3dce304a7679f808261edcd4422be6
michael-mbugua <kamirimichael369@gmail.com>
2023-06-07 12:40:57 +0300

    create controller actions

--- app/controllers/bags_controller.rb
+++ app/controllers/bags_controller.rb
@@ -3,4 +3,31 @@ class BagsController < ApplicationController
         bag=Bag.all;
         render json: bag, status: :ok
     end
+
+    def show
+        bag=Bag.find(params[:id]);
+        render json: bag, status: :found
+    end
+
+    def create
+        bag=Bag.create(bags_params)
+        render json: bag, status: :created
+    end
+
+
+    def destroy
+        bag= Bag.find(params[:id])
+        bag.destroy
+    end
+
+    def update
+        bag=Bag.find(params[:id])
+        bag.update(bags_params)
+        render json: bag
+    end
+
+    private
+    def bags_params
+        params.permit(:name,:bagType,:name,:image,:price,:available,:description)
+    end
 end
