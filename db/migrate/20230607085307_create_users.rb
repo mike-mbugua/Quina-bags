6751bac68c00bedef85cc4261e2b6d353ca349c4
michael-mbugua <kamirimichael369@gmail.com>
2023-06-07 12:11:30 +0300

    create user table

--- db/migrate/20230607085307_create_users.rb
+++ db/migrate/20230607085307_create_users.rb
@@ -1,7 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-
+      t.string :firstName
+      t.string :lastName
+      t.string :username
+      t.string :email
+      t.integer :phone
+      t.text :password_digest
       t.timestamps
     end
   end
