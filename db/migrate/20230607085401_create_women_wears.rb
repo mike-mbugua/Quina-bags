f05eaa03a3f62174336413b3e7807cd197502bb3
michael-mbugua <kamirimichael369@gmail.com>
2023-06-07 12:12:04 +0300

    create womenWear table

--- db/migrate/20230607085401_create_women_wears.rb
+++ db/migrate/20230607085401_create_women_wears.rb
@@ -1,6 +1,12 @@
 class CreateWomenWears < ActiveRecord::Migration[7.0]
   def change
     create_table :women_wears do |t|
+      t.string :weartype
+      t.string :image
+      t.integer :price
+      t.text :size
+      t.boolean :available
+      t.text :description
 
       t.timestamps
     end
